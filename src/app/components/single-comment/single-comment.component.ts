5d8e60f189215c07d3ec70c59d351163a915f916
Federica Chiantini <fede.chiantini@gmail.com>
2024-07-13 10:25:39 +0200

    add HttpErrorResponse

--- src/app/components/single-comment/single-comment.component.ts
+++ src/app/components/single-comment/single-comment.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommentsService } from '../../services/comments.service';
 import { DataComment } from '../../models/types';
 import { ActivatedRoute } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-single-comment',
@@ -17,10 +18,10 @@ export class SingleCommentComponent implements OnInit{
     if(this.serviceSingle.postId){
       this.serviceSingle.postId = false;
       const Pid = this.route.snapshot.paramMap.get("postId")!
-      this.serviceSingle.getGroupComments(+Pid).subscribe(p => this.Comment = p)
+      this.serviceSingle.getGroupComments(+Pid).subscribe({next: (dati) => {this.Comment = dati}, error: (error : HttpErrorResponse)=>{console.log("errore:" +""+ error.status)}})
     }else{      
       const id = this.route.snapshot.paramMap.get("id")!
-      this.serviceSingle.getSingleComment(id).subscribe( p => this.Comment = p) 
+      this.serviceSingle.getSingleComment(id).subscribe({next: (dati) => {this.Comment = dati}, error: (error : HttpErrorResponse)=>{console.log("errore:" +""+ error.status)}})
     }
   }
 }
